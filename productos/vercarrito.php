d33c4ef30381d721ba20a59d169d7a2d9a383947
CarlosV <carlillos60ive.com.mx>
2020-06-04 23:41:16 -0500

    actualizar

---
+++ productos/vercarrito.php
@@ -0,0 +1,26 @@
+<?php
+include "../acceso/conexion.php";
+$result = mysqli_query($conecta,"SELECT * FROM carrito");
+
+echo "<table border='1'>";
+
+$i = 0;
+while($row = $result->fetch_assoc())
+{
+    if ($i == 0) {
+      $i++;
+      echo "<tr>";
+      foreach ($row as $key => $value) {
+        echo "<th>" . $key . "</th>";
+      }
+      echo "</tr>";
+    }
+    echo "<tr>";
+    foreach ($row as $value) {
+      echo "<td>" . $value . "</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+
+?>
